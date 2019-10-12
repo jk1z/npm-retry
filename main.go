406c923145ddcd26e7f3f9388bedea095ee5269b
Jack Zhang <18542127+zZFreezyZz@users.noreply.github.com>
2019-10-12 12:58:45 +1100

    Minor fix on npm path

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ import (
 func main(){
 	dir, _ := os.Getwd()
 	if checkFileExists(dir + "/package-lock.json") && checkFileExists(dir + "/package.json"){
+		npmPath, err := exec.LookPath("npm")
+		if err != nil {
+			log.Fatal(err)
+		}
 		completed := false
 		for i:= 0; i < 5; i++ {
 			fmt.Printf("Trying to npm ci in %s... This cmd will timeout in 3 minutes\n", dir)
-			cmd := exec.Command("bash", "-c", "npm ci")
+			cmd := exec.Command(npmPath, "ci")
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			err := cmd.Start()
@@ -32,10 +36,19 @@ func main(){
 					if err := cmd.Process.Kill(); err != nil {
 						log.Fatal("Failed to kill process: ", err)
 					}
-				case <- done:
-					completed = done == nil
+				case err := <- done:
+					if err != nil{
+						waitErr := err.Error()
+						log.Println("Failed to execute npm ci. Err:", waitErr)
+					} else {
+						completed = true
+					}
+
 			}
-			if completed { return }
+			if completed {
+				log.Println("Successfully executed npm ci")
+				return
+			}
 		}
 		log.Fatal("Failed to execute npm ci for 5 times")
 	} else {
